f3b77947a65c876f02af8473b68889f53da502db
Jihee Eom <bonjenny1@gmail.com>
2025-12-29 19:23:25 +0900

    2 dive to ast

---
+++ src/rules/no-function-apply.ts
@@ -0,0 +1,19 @@
+/* src/rules/no-function-apply.ts */
+
+import { Rule } from "eslint";
+import { Node } from "estree";
+
+const rule: Rule.RuleModule = {
+  create: context => {
+    return {
+      "CallExpression > MemberExpression > Identifier.property[name='apply']": (node: Node) => {
+        context.report({
+          message: "Don't use 'apply'",
+          node,
+        });
+      },
+    };
+  },
+};
+
+export = rule;
